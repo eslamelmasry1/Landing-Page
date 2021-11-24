bd960fddb0dc9c34d8aae41cc1de0da9339b04d6
eslamelmasry1 <89648925+eslamelmasry1@users.noreply.github.com>
2021-11-24 16:49:56 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,8 +25,29 @@
 */
 const navMenu = document.querySelector('#navbar__list');
 const sections = Array.from(document.querySelectorAll("section"));
+
 /**
  * End Global Variables
+ * Begin Main Functions
+ * Create a function with for...of loop inside.
+ * Inside the for...of loop, create <li> element for each section.
+ * Inside each <li> element add the anchor links and class by using innerHTML to link it to the correspondant section.
+ * Use appendChild to store the <li> elements to the DOM.
+*/
+// Build menu 
+
+
+sections.forEach(function createNavBarList(section) {
+    list = document.createElement("li");
+    id = section.id
+    menuLink = section.dataset.nav;
+    list.innerHTML = `<a href="#${id}" class="menu__link">${menuLink}</a>`;
+    navMenu.appendChild(list);
+});
+
+
+/**
+ * End Main Functions
  * Start Helper Functions
 
 */
@@ -36,43 +57,19 @@ function viewport(element) {
     return(position.top >= 0);
 }
 // Set sections as active
-function activeState() {
-    for(sect of sections) {
-        if(viewport(sect)) {
-                sect.classList.add('your-active-class');
-        } else {
-            sect.classList.remove('your-active-class');
-        }
-    }
-}
-// Scroll to section on link click
-document.addEventListener('scroll', activeState);
+
 
 /**
  * End Helper Functions
- * Begin Main Functions
- * Create a function with for...of loop inside.
- * Inside the for...of loop, create <li> element for each section.
- * Inside each <li> element add the anchor links and class by using innerHTML to link it to the correspondant section.
- * Use appendChild to store the <li> elements to the DOM.
-*/
-// Build menu 
-function createNavBarList() {
-    for (sect of sections) {
-        // build the nav
-        navBarList = document.createElement("li");
-        itemId = sect.getAttribute("id");
-        itemName = sect.getAttribute("data-nav");
-
-        navBarList.innerHTML = `<a href="#${itemId}" data-nav="${itemName}" class="menu__link">${itemName}</a>`;
-        navMenu.appendChild(navBarList);
-    }    
-}
-
-createNavBarList();
-
-/**
- * End Main Functions
  * Begin Events
  * 
 */
+document.addEventListener('scroll', function activeState() {
+    for(section of sections) {
+        if(viewport(section)) {
+            section.classList.add('your-active-class');
+        } else {
+            section.classList.remove('your-active-class');
+        }
+    }
+});
