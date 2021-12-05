dfb4c79f2249a156f3da7e7251d4baf7f5081fc2
eslamelmasry1 <89648925+eslamelmasry1@users.noreply.github.com>
2021-12-05 18:54:37 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,75 +1,63 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+const navBarList = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
 
-/**
- * Define Global Variables
- * Store the menu list in a variable
- * Store all the sections as an array in a variable to use it in the function I'll be creating afterwards.
-*/
-const navMenu = document.querySelector('#navbar__list');
-const sections = Array.from(document.querySelectorAll("section"));
-
-/**
- * End Global Variables
- * Begin Main Functions
- * Create a forEach loop from section variable I've created before.
- * Inside the forEach loop, I've created a function that consists of creating <li> tag for each section.
- * Inside each <li> tag add the anchor links and class by using innerHTML to link it to the correspondant section.
- * Use appendChild to store the <li> tags to the DOM.
-*/
-// Build menu 
+//Creating a function to create a list items depending on the amount of sections we have in the HTML file.
+//The more sections we add in the HTML file, the more list items we will get in the nav bar.
 
 
-sections.forEach(function createNavBarList(section) {
-    list = document.createElement("li");
-    id = section.id
-    menuLink = section.dataset.nav;
-    list.innerHTML = `<a href="#${id}" class="menu__link">${menuLink}</a>`;
-    navMenu.appendChild(list);
+sections.forEach(function (section) {
+    listElements = document.createElement("li");
+    //Another way to run the same function.
+    /*let anchor = document.createElement('a');   
+    anchor.href = "#" + `${section.id}`;
+    anchor.textContent = `${section.dataset.nav}`;
+    anchor.classList.add('menu__link');
+    listElements.append(anchor);*/
+
+    //I could've use the above code, but for performance purposes, I prefer to use one line code as the following.
+    listElements.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
+    navBarList.appendChild(listElements);
 });
 
+//Storing all li tags in a variable to use it later.
+let menuList = document.querySelectorAll('li');
 
-/**
- * End Main Functions
- * Start Helper Functions
+//Adding event listener that when scrolling runs the following function
+window.addEventListener('scroll', function () {
+    //Using for loop to apply the code on each section
+    for(let i = 0; i < sections.length; i++) {
+       
+        //Using getBoundingClientRect().top to get the the exact position in the page when scrolling
+        rectTop = sections[i].getBoundingClientRect().top;
 
-*/
-// Add class 'active' to section when near top of viewport
-function viewport(element) {
-    const position = element.getBoundingClientRect();
-    return(position.top >= 0);
-}
-// Set sections as active
-
-
-/**
- * End Helper Functions
- * Begin Events
- * Add event listener to scroll to relevant section on link click., by using a function that activates each section once clicking it on the nav bar. 
-*/
-document.addEventListener('scroll', function activeState() {
-    for(section of sections) {
-        if(viewport(section)) {
-            section.classList.add('your-active-class');
+        //Using conditional to add your-active-class when it hits the top of each section
+        if(rectTop < window.innerHeight) {
+            sections[i].classList.add('your-active-class');
         } else {
-            section.classList.remove('your-active-class');
+            //Removing your-active-class when it doesn't fulfill the first condition.
+            sections[i].classList.remove('your-active-class');
+        }
+
+        //Using conditional to highlight the relevant navbar element when it hits the top of each relevant section
+        let offsetTop = sections[i].offsetTop;
+        let offsetHeight = sections[i].offsetHeight;
+
+        if(offsetTop <= window.scrollY && offsetTop + offsetHeight > window.scrollY) {
+            menuList[i].style.background="#333";
+            menuList[i].style.color="#fff";
+            menuList[i].style.transition="ease 0.3s all";
+            
+        } else {
+            menuList[i].style.background="white";
+            menuList[i].style.color="#000";
         }
     }
 });
+
+//Adding event listener to scroll to Top button
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    window.scrollTo(0, 0)
+})
