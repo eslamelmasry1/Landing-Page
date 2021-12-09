7d8a6e53d5db89e69851b8c13531b54f70cf01ba
eslamelmasry1 <89648925+eslamelmasry1@users.noreply.github.com>
2021-12-09 10:33:48 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@
 const navBarList = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
 
+
 //Creating a function to create a list items depending on the amount of sections we have in the HTML file.
 //The more sections we add in the HTML file, the more list items we will get in the nav bar.
-
 
 sections.forEach(function (section) {
     listElements = document.createElement("li");
     //I could've use the above code, but for performance purposes, I prefer to use one line code as the following.
-    listElements.innerHTML = `<a href="#${section.id}" data="${section.id}" class="menu__link">${section.dataset.nav}</a>`;
+    listElements.innerHTML = `<a data="${section.id}" class="menu__link">${section.dataset.nav}</a>`;
     navBarList.appendChild(listElements);
 });
 
-//Storing all li tags in a variable to use it later.
+//Storing all anchor tags in a variable to use it later
 let menuList = document.querySelectorAll('a');
 
 //Using forEach loop to add event listener for each anchor link.
@@ -29,39 +29,49 @@ menuList.forEach( (menuItem) => {
     });
 });
 
+
+
 //Adding event listener that when scrolling runs the following function
 window.addEventListener('scroll', function () {
+    
+    //Storing all li tags in a variable to use it later
+    let links = document.querySelectorAll('Li');
+
     //Using for loop to apply the code on each section
     for(let i = 0; i < sections.length; i++) {
-       
-        //Using getBoundingClientRect().top to get the the exact position in the page when scrolling
+
+        //Using getBoundingClientRect().top and getBoundingClientRect().bottom to get the exact position in the page when scrolling
         rectTop = sections[i].getBoundingClientRect().top;
+        rectBottom = sections[i].getBoundingClientRect().bottom;
 
         //Using conditional to add your-active-class when it hits the top of each section
-        if(rectTop < window.innerHeight) {
+        if(rectTop + rectBottom > window.innerHeight) {
             sections[i].classList.add('your-active-class');
         } else {
             //Removing your-active-class when it doesn't fulfill the first condition.
             sections[i].classList.remove('your-active-class');
         }
+    }
 
-        //Using conditional to highlight the relevant navbar element when it hits the top of each relevant section
-        let offsetTop = sections[i].offsetTop;
-        let offsetHeight = sections[i].offsetHeight;
-
-        if(offsetTop <= window.scrollY && offsetTop + offsetHeight > window.scrollY) {
-            menuList[i].style.background="#333";
-            menuList[i].style.color="#fff";
-            menuList[i].style.transition="ease 0.3s all";
-            
+    //Using conditional to highlight the relevant navbar element when it hits the top of each relevant section
+    for(let j = 0; j < links.length; j++) {
+        links[j].classList.remove('active');
+    }
+    for(let k = 0; k < links.length; k++) {
+        if(scrollY > 2100) {
+            links[3].classList.add('active');
+        } else if(scrollY > 1500 && scrollY < 2100) {
+            links[2].classList.add('active');
+        } else if(scrollY > 900 && scrollY < 1500) {
+            links[1].classList.add('active');
+        } else if(scrollY > 366 || scrollY < 900) {
+            links[0].classList.add('active');
         } else {
-            menuList[i].style.background="white";
-            menuList[i].style.color="#000";
+            links[k].classList.remove('active');
         }
+        
     }
 });
-
-
 
 
 //Adding event listener to scroll to Top button
